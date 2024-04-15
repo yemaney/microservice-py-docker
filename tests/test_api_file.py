6c0887659ff31d3575b2d7cc40cb50d9fa13da60
yemaney <yemane_@outlook.com>
2024-04-15 04:22:09 +0000

    ✅ : pass api file test

--- tests/test_api_file.py
+++ tests/test_api_file.py
@@ -3,27 +3,21 @@ from typing import List, Tuple
 
 import pytest
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
 from sqlmodel import Session
 
 from api.core import models
-from api.core.database import get_session
-from api.main import app
+
 
 
 @pytest.mark.anyio
-async def test_upload_file(session: Session, logged_in_user: Tuple[dict, List[models.UserCreate]]):
-    def get_session_override():
-        return session
+async def test_upload_file(client: TestClient, session: Session, logged_in_user: Tuple[dict, List[models.UserCreate]]):
 
-    app.dependency_overrides[get_session] = get_session_override
     jwt = logged_in_user[0]["access_token"]
     headers = {"Authorization": f"Bearer {jwt}"}
 
     with tempfile.TemporaryFile() as fp:
         files = [("file", ("my_file.txt", fp, "text/plain"))]
-        async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
-            response = await ac.post("/files/", headers=headers, files=files, data={})
+        response = client.post("/files", headers=headers, files=files)
 
         data = response.json()
         assert response.status_code == 201
